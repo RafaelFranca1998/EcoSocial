2d80761e531847db3f51511ab379ccf55239a0e1
Rafael França <rafaelfranca2013@hotmail.com>
2018-09-06 00:32:42 -0300

    Initial commit

---
+++ app/src/main/java/com/example/rafael_cruz/prototipo/activity/MainActivity.java
@@ -0,0 +1,142 @@
+package com.example.rafael_cruz.prototipo.activity;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.rafael_cruz.prototipo.R;
+import com.example.rafael_cruz.prototipo.fragments.AboutFragment;
+import com.example.rafael_cruz.prototipo.fragments.MainFragment;
+import com.example.rafael_cruz.prototipo.fragments.MapsFragment;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    NavigationView navigationView;
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        setToolbarTitle("Inicio");
+        setSupportActionBar(toolbar);
+        //Set the fragment Initially
+        MainFragment fragment = new MainFragment();
+        FragmentTransaction fragmentTransaction =
+                getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+//            SettingsFragment fragment = new SettingsFragment();
+//            android.support.v4.app.FragmentTransaction fragmentTransaction =
+//                    getSupportFragmentManager().beginTransaction();
+//            fragmentTransaction.replace(R.id.fragment_container, fragment);
+//            fragmentTransaction.commit();
+            return true;
+        }else if (id == R.id.action_about){
+            AboutFragment fragment = new AboutFragment();
+            android.support.v4.app.FragmentTransaction fragmentTransaction =
+                    getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.commit();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_principal) {
+            MainFragment fragment = new MainFragment();
+            android.support.v4.app.FragmentTransaction fragmentTransaction =
+                    getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.commit();
+        } else if (id == R.id.nav_eventos) {
+            MapsFragment fragment= new MapsFragment();
+         //   setToolbarTitle("Eventos");
+            android.support.v4.app.FragmentTransaction fragmentTransaction =
+                    getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.commit();
+        } else if (id == R.id.nav_marcar_evento) {
+//            SelectOnMapFragment fragment= new SelectOnMapFragment();
+//            android.support.v4.app.FragmentTransaction fragmentTransaction =
+//                    getSupportFragmentManager().beginTransaction();
+//            fragmentTransaction.replace(R.id.fragment_container, fragment);
+//            fragmentTransaction.commit();
+
+            Intent intent =  new Intent(this,AddEventActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    /**
+     * Usado para modificar o titulo na barra de titulo.
+     * @param title
+     */
+    public void setToolbarTitle(String title){
+        if (toolbar != null) {
+            toolbar.setTitle(title);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/rafael_cruz/prototipo/fragments/MainFragment.java
@@ -0,0 +1,106 @@
+package com.example.rafael_cruz.prototipo.fragments;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.example.rafael_cruz.prototipo.config.AdapterListView;
+import com.example.rafael_cruz.prototipo.config.ItemListView;
+import com.example.rafael_cruz.prototipo.R;
+import com.example.rafael_cruz.prototipo.model.Eventos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends Fragment {
+    private ListView            listView;
+    private Context             context;
+    private Toolbar             toolbar;
+    private String              FINAL_DESCRICAO      = "Descrição: ";
+    private String              FINAL_LOCALIDADE     = "Localidade: ";
+    private ItemListView        itemListView;
+    private List<ItemListView>  itens;
+
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        listView = rootView.findViewById(R.id.list);
+
+
+        Eventos eventos =  new Eventos(10,10,"10","10","10",100000,10000);
+
+       // String[] dados = new String[] { "Cupcake", "Donut", "Eclair", "Froyo", "Gingerbread",
+       //         "Honeycomb", "Ice Cream Sandwich", "Jelly Bean",
+       //         "KitKat", "Lollipop", "Marshmallow", "Nougat" };
+
+       // ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, dados);
+
+      //  listView.setAdapter(adapter);
+
+        itemListView =  new ItemListView();
+
+        itens = new ArrayList<ItemListView>();
+
+        addEvent("Cachorro perdido","Federação",R.drawable.icon_cachorro_perdido);
+
+        addEvent("Coleta de Lixo","Ribeira",R.drawable.icons8_rss_50);
+
+        AdapterListView adapterListView =  new AdapterListView(context,itens);
+
+        listView.setAdapter(adapterListView);
+      //  ((MainActivity) getActivity()).setToolbarTitle("Inicio");
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(context,"nada ainda",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return rootView;
+
+    }
+
+    /**
+     * Metodo para adicionar eventos.
+     * @param descricao
+     * @param localidade
+     * @param imagem
+     */
+    public void addEvent(String descricao, String localidade, int imagem){
+        itemListView =  new ItemListView();
+
+        itemListView.setTextoDescricao(FINAL_DESCRICAO+descricao);
+
+        itemListView.setTextoLocalidade(FINAL_LOCALIDADE+localidade);
+
+        itemListView.setIconeRid(imagem);
+
+        itens.add(itemListView);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        this.context = context;
+        super.onAttach(context);
+    }
+}
+
+
